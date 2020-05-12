194d5a10a0d5f00bf430982d5dafd19549817e84
Halo_7777777 <1144265174@qq.com>
2020-05-12 14:31:41 +0800

    server version2.0

--- server/server.py
+++ server/server.py
@@ -1,4 +1,33 @@
+# -*- coding: utf-8 -*-
 import socket
+import signal
+import os
+import errno
+
+
+def main():
+    try:
+        # 默认绑定服务器本机10001端口
+        s.bind(('0.0.0.0', 10001))
+
+        # 服务器每5s尝试回收票据
+        signal.signal(signal.SIGALRM, ticket_reclaim)
+        signal.alarm(5)
+
+        while True:
+            try:
+                print('\nwait for datagram......')
+                data, addr = s.recvfrom(1024)
+                print('accept new datagram from {}'.format(addr))
+                narrate("GOT:", data.decode(), ('0.0.0.0', 10001))
+                time_left = signal.alarm(0)  # 正常操作中关闭alarm
+                handle_request(data, addr)
+                signal.alarm(time_left)
+            except ConnectionResetError:
+                print("connection reset")
+    except KeyboardInterrupt:
+        print('manual exit')
+    s.close()
 
 
 # 处理请求
@@ -12,7 +41,7 @@ def handle_request(req, client):
         response = do_validate(req)
     else:
         response = 'FAIL invalid request'
-    response = narrate("SAID:", response, client)
+    narrate("SAID:", req, client)
     ret = s.sendto(response.encode(encoding='utf-8'), client)
     if ret == -1:
         print("SERVER sendto failed")
@@ -22,7 +51,7 @@ def handle_request(req, client):
 def do_hello(req):
     global num_tickets_out
     x = 0
-    if len(ticket_array) >= MAXUSERS:
+    if num_tickets_out >= MAXUSERS:
         return 'FAIL no tickets available'
 
     # find the sequence of avail_ticket
@@ -37,7 +66,8 @@ def do_hello(req):
         return 'FAIL database corrupt'
 
     # generate ticket of form: pid.slot
-    ticket_array.append(req[5:])
+    ticket_array[x] = int(req[5:])
+    write_data(ticket_database)
     replybuf = 'TICK {}.{}'.format(req[5:], str(x))
     num_tickets_out += 1
     return replybuf
@@ -48,11 +78,13 @@ def do_goodbye(req):
     try:
         index = req.index('.')
         pid = req[5:index]
-        slot = req[index+1:]
+        slot = req[index + 1:]
         if (int(slot) >= num_tickets_out) or (ticket_array[int(slot)] != pid):
             raise ValueError
     except ValueError:
         return 'FAIL invalid ticket'
+    ticket_array[int(slot)] = TICKET_AVAIL
+    write_data(ticket_database)
     return 'THNX See ya!'
 
 
@@ -61,31 +93,69 @@ def do_validate(req):
     try:
         index = req.index('.')
         pid = req[5:index]
-        slot = req[index:]
+        slot = req[index + 1:]
         if (int(slot) >= num_tickets_out) or (ticket_array[int(slot)] != pid):
             raise ValueError
     # bad ticket
     except ValueError:
-        narrate('Bogus ticket', req[5:], None)
+        narrate('Bogus ticket ', req[5:], None)
         return 'FAIL invalid ticket'
     # good ticket
     return 'GOOD Valid ticket'
 
 
 # 处理服务器返回的叙述
-def narrate(title, response, client):
-    response = 'SERVER:{}{} {}'.format(title, response, client)
+def narrate(title, req, client):
+    response = 'SERVER:{}{} {}'.format(title, req, client)
     print(response)
-    return response
+
+
+# 收回丢失的票据
+def ticket_reclaim(signum, frame):
+    global num_tickets_out
+    for i in range(0, MAXUSERS):
+        if ticket_array[i] != TICKET_AVAIL:
+            try:
+                os.kill(ticket_array[i], 0)
+            except OSError as e:
+                if e.errno == errno.ESRCH:
+                    tick = '{}.{}'.format(ticket_array[i], i)
+                    narrate("freeing ", tick, None)
+                    ticket_array[i] = TICKET_AVAIL
+                    write_data(ticket_database)  # 即时更新数据表
+                    num_tickets_out -= 1
+    # reset alarm clock
+    signal.alarm(5)
+
+
+# 读取数据
+def read_data(filename):
+    i = 0
+    with open(filename) as file_object:
+        for line in file_object:
+            ticket_array[i] = int(line)
+            i += 1
+
+
+# 存储数据
+def write_data(filename):
+    with open(filename, 'w') as file_object:
+        for ticket in ticket_array:
+            file_object.write(str(ticket) + '\n')
 
 
 if __name__ == '__main__':
+
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # 默认绑定服务器本机10001端口
-    s.bind(('0.0.0.0', 10001))
+    # 票据数据存储
+    ticket_database = 'tickets.txt'
+
+    # 暂定最大用户数量1000
+    MAXUSERS = 1000
+
     # 票据列表
-    ticket_array = []
+    ticket_array = [0] * MAXUSERS
 
     # 可用于分配的票据标记
     TICKET_AVAIL = 0
@@ -93,16 +163,7 @@ if __name__ == '__main__':
     # 已经分配的票据数量
     num_tickets_out = 0
 
-    # 暂定最大用户数量1000
-    MAXUSERS = 1000
-    try:
-        while True:
-            print('\nwait for datagram......')
-            data, addr = s.recvfrom(1024)
-            print(f"accept new datagram from {addr}")
-            handle_request(data, addr)
-    except ConnectionResetError:
-        print("Error")
-    except KeyboardInterrupt:
-        print('quiting')
-    s.close()
+    # 预处理读取数据文件
+    read_data(ticket_database)
+
+    main()
